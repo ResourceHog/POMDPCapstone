4fc801914a602e0ad4772084be3398170af0b16c
Travis Rivera <TravisGRivera@gmail.com>
2017-06-07 20:04:36 -0700

    should have fixed merge issues

--- visualtestscripts.py
+++ visualtestscripts.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 """
 Created on Sun May 21 19:40:11 2017
@@ -9,16 +8,4 @@ Created on Sun May 21 19:40:11 2017
 # Import the visualization code
 import visuals as vs
 
-=======
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May 21 19:40:11 2017
-
-@author: ECOWIZARD
-"""
-
-# Import the visualization code
-import visuals as vs
-
->>>>>>> df11c878d6e5aaae814194438e7a769c07156d06
 vs.plot_trials('sim_no-learning.csv')
