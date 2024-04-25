df990ea2538efdce0cf88507964674e2a7d5870a
Travis Rivera <TravisGRivera@gmail.com>
2024-04-24 21:21:40 -0400

    separated 1x4 and tigersim

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ from Environment4x1 import Environment4x1
 from pomdpagent import LearningAgent
 from agent import LearningAgent as Qlearner
 
+TIGERSIM = False
 
 def run():
-    """ Driving function for running the simulation. 
+    """ Driving function for running the simulation.
         Press ESC to close the simulation, or [SPACE] to pause the simulation. """
 
     ##############
@@ -23,8 +24,11 @@ def run():
     #   verbose     - set to True to display additional output from the simulation
     #   num_dummies - discrete number of dummy agents in the environment, default is 100
     #   grid_size   - discrete number of intersections (columns, rows), default is (8, 6)
-    #env = EnvironmentTiger()
-    fourby1env = Environment4x1()
+    env = None
+    if TIGERSIM:
+        env = EnvironmentTiger()
+    else:
+        env = Environment4x1()
     ##############
     # Create the driving agent
     # Flags:
@@ -35,14 +39,14 @@ def run():
     wepsilon = 0.9999
     walpha = 0.3
     wresolution = 4
-    #agent = env.create_agent(LearningAgent,learning = isLearning, epsilon = wepsilon, alpha=walpha,resolution=wresolution)
-    agent4x1 = fourby1env.create_agent(LearningAgent, learning = isLearning, epsilon = wepsilon,alpha = walpha,resolution=wresolution)
+    agent = env.create_agent(LearningAgent,learning = isLearning, epsilon = wepsilon, alpha=walpha,resolution=wresolution)
+    #agent4x1 = env.create_agent(LearningAgent, learning = isLearning, epsilon = wepsilon,alpha = walpha,resolution=wresolution)
     ##############
     # Follow the driving agent
     # Flags:
     #   enforce_deadline - set to True to enforce a deadline metric
-    #env.set_primary_agent(agent)
-    fourby1env.set_primary_agent(agent4x1)
+    env.set_primary_agent(agent)
+    #env.set_primary_agent(agent4x1)
     ##############
     # Create the simulation
     # Flags:
@@ -53,37 +57,44 @@ def run():
     isLogging = True
     isOptimised = False
     delay = 0.01
-    
-    #sim = TigerSimulator(env,update_delay = delay,log_metrics=isLogging    ,optimized=isOptimised)
-    
+    sim = None
+    if TIGERSIM:
+        sim = TigerSimulator(env,update_delay = delay,log_metrics=isLogging    ,optimized=isOptimised)
+    else:
+        sim = Simulator4x1(env,update_delay = delay,log_metrics=isLogging    ,optimized=isOptimised)
+
     ##############
     # Run the simulator
     # Flags:
-    #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
+    #   tolerance  - epsilon tolerance before beginning testing, default is 0.05
     #   n_test     - discrete number of testing trials to perform, default is 0
     #sim.run(n_test = 50)
-    
-    
-    
-    
     print("starting second sim")
-    sim4x1 = Simulator4x1(fourby1env,update_delay = delay,log_metrics=isLogging    ,optimized=isOptimised)
-    sim4x1.run(n_test = 2)
-    
-    
+    #sim4x1 = Simulator4x1(env,update_delay = delay,log_metrics=isLogging,optimized=isOptimised)
+    sim.run(n_test = 2)
+
+
     print("starting control.")
     #agent = env.create_agent(Qlearner,learning = isLearning, epsilon = wepsilon, alpha=walpha)
-    agent4x1 = fourby1env.create_agent(Qlearner,learning = isLearning, epsilon = wepsilon, alpha = walpha)
+    agent = env.create_agent(Qlearner,learning = isLearning, epsilon = wepsilon, alpha = walpha)
     #sim = env.set_primary_agent(agent)
-    fourby1env.set_primary_agent(agent4x1)
+    env.set_primary_agent(agent)
+
+
+
+    if TIGERSIM:
+        sim = TigerSimulator(env,update_delay = delay,log_metrics=isLogging    ,optimized=isOptimised,vanilla = True)
+    else:
+        sim = Simulator4x1(env,update_delay = delay,log_metrics=isLogging    ,optimized=isOptimised,vanilla = True)
+    sim.run(n_test = 2)
     #sim = TigerSimulator(env,update_delay = delay ,log_metrics = isLogging,optimized=isOptimised,vanilla = True)
     #sim.run(n_test = 50)
-    
-    
-    
+
+
+
     print("starting second sim")
-    sim4x1 = Simulator4x1(fourby1env,update_delay = delay,log_metrics=isLogging    ,optimized=isOptimised,vanilla = True)
-    sim4x1.run(n_test = 50)    
+    # sim = Simulator4x1(env,update_delay = delay,log_metrics=isLogging    ,optimized=isOptimised,vanilla = True)
+    # sim.run(n_test = 2)
 
 if __name__ == '__main__':
     run()
